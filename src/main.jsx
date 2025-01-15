1470bcfc9bffe76851215e25b6f4322f7312f176
Burhan <burhanchughtai744@gmail.com>
2025-01-16 00:48:49 +0500

    'updated-code'

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { GlobalProvider } from './context/GlobalState.jsx'
+import { GlobalProvider } from './context/GlobalState'
 
 createRoot(document.getElementById('root')).render(
   <GlobalProvider>
